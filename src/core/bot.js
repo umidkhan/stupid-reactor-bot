2ec63baff836c31569fdea3fd0b49e972bd72f9f
umidkhan <umidxondeveloper@gmail.com>
2024-11-13 20:16:02 +0500

    first commit

---
+++ src/core/bot.js
@@ -0,0 +1,58 @@
+const { Telegraf } = require("telegraf");
+require("dotenv").config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start(async ctx => {
+    ctx.reply(`Welcome to the bot, stupid <a href="tg://user?id=${ctx.from.id}" >${ctx.from.first_name}</a> 😜`, {
+        reply_to_message_id: ctx.message.message_id,
+        parse_mode: "HTML"
+    })
+});
+
+bot.on("channel_post", ctx => {
+    if (ctx.channelPost) {
+        return null;
+    }
+})
+
+bot.on("text", async ctx => {
+    if (ctx.msg.text.includes("⚡️")) {
+        await ctx.react("⚡️")
+    } else if (ctx.msg.text.includes("🔥")) {
+        await ctx.react("🔥")
+    } else if (ctx.msg.text.includes("💯")) {
+        await ctx.react("💯")
+    } else if (ctx.msg.text.includes("stupid")) {
+        await ctx.reply(`No, you're stupid <a href="tg://user?id=${ctx.from.id}" >${ctx.from.first_name}</a> 😁`, {
+            reply_to_message_id: ctx.message.message_id,
+            parse_mode: "HTML"
+        })
+    } else if (ctx.msg.text.includes("idiot")) {
+        await ctx.reply(`No, you're idiot <a href="tg://user?id=${ctx.from.id}" >${ctx.from.first_name}</a> 😁`, {
+            reply_to_message_id: ctx.message.message_id,
+            parse_mode: "HTML"
+        })
+    } else if (ctx.msg.text.includes("snow")) {
+        await ctx.reply(`Do you like a snow? 😁`, {
+            reply_to_message_id: ctx.message.message_id,
+            parse_mode: "HTML"
+        })
+    }
+    else {
+        await ctx.react("❤️")
+    }
+})
+
+bot.on("message", async ctx => {
+    await ctx.react("❤️")
+})
+
+
+bot.catch(err => {
+    console.error(err);
+})
+
+bot.launch(() => {
+    console.log("-! Bot started !-")
+})
---
+++ src/modules/cron.js
@@ -0,0 +1,16 @@
+const cron = require("node-cron");
+const axios = require("axios");
+require("dotenv").config()
+
+const task = cron.schedule("* * * * *", async () => {
+    try {
+        const response = await axios.post(
+            process.env.WEBHOOK_URI
+        );
+        console.log(`POST reqest sent: ${response.data}`);
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+module.exports = task;
